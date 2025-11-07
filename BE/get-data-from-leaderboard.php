532acc0f1ec5d0afc68100706b042a99e40f1e98
Fadi nasr <nasr.fadi2511@gmail.com>
2025-11-07 23:28:56 +0200

    Fixing some bugs

--- BE/get-data-from-leaderboard.php
+++ BE/get-data-from-leaderboard.php
@@ -2,14 +2,14 @@
 
 include ("connection.php");
 
-$sql="SELECT name,score,duration FROM leaderboard ORDER BY score DESC";
-$query=$mysqli->prepare($sql);
+$sql="SELECT name,score,duration FROM leaderboard ORDER BY score DESC LIMIT 5" ;
+$query=$mysqli->prepare($sql);   
 $query->execute();
 
 $array=$query->get_result();
 $response=[];
-while($article=$array->fetch_assoc()){
-    $response[]=$article;
+while($a=$array->fetch_assoc()){
+    $response[]=$a;
 }
 
 echo json_encode($response);
