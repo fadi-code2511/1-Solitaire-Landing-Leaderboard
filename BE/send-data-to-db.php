eb59123617bb780cbc3ac96d60f935cc687f999b
Fadi nasr <nasr.fadi2511@gmail.com>
2025-11-07 23:51:56 +0200

    Intializing API for send the data to db

---
+++ BE/send-data-to-db.php
@@ -0,0 +1,20 @@
+<?php
+include("connection.php");
+
+$name = $_POST['name'];
+$score = rand(50, 200); //  random score
+$duration = rand(1, 4) . ":" . rand(0, 59); // random duration
+
+$sql = "INSERT INTO leaderboard (name, score,duration) 
+        VALUES (?,?,?)";
+
+$query = $mysqli->prepare($sql);
+$query->bind_param("sis",$name,$score,$duration);  //we use it for the sql injection
+$query->execute();
+
+if ($query) {
+    echo json_encode(["message" => "Score added"]);
+} else {
+    echo json_encode(["error" => $mysqli->error]);
+}
+
